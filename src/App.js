5a4249b8620c4efd11591d3bb27ca6842f9d8a4d
pparksang1013 <pparksang1013@users.noreply.github.com>
2023-04-13 12:16:33 +0900

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// ROUTE
+import Main from "./router/Main";
+import Signup from "./router/Signup";
+import Signin from "./router/Signin";
+import Todo from "./router/Todo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/signin" element={<Signin />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                    <Route path="/todo" element={<Todo />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
+
+// localStorage.getItem("access_token") ? (
+//     <Navigate replace to="/todo" />
+// ) : (
+//     <SignIn />
+// )
